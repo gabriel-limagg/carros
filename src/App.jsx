2392af70315634e15f3d4445e2ff2672ec20dbdd
gabriel lima <gabriellima140806@gmail.com>
2024-03-17 03:31:05 -0300

    quarto commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,22 +8,22 @@ export default function App() {
       <section>
       <div className="bg-orange">
         <img src={iconSedan} alt="" />
-            <h2 className="">Sedans</h2>
-            <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
-              <a href="https://www.webmotors.com.br/sedans/carros/estoque?tipoveiculo=carros&necessidade=Sedans" target="_blank">ver mais</a>
+            <h2 className="font-shouIders">Sedans</h2>
+            <p className="font-lexend">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
+              <a className="font-lexend" href="https://www.webmotors.com.br/sedans/carros/estoque?tipoveiculo=carros&necessidade=Sedans" target="_blank">ver mais</a>
       </div>
        
       <div className="bg-green">
             <img src={iconSuvs} alt="" />
-            <h2 className="text-4xl">SUVs</h2>
-            <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
-            <a href="https://www.webmotors.com.br/suvs" target="_blank" >ver mais</a>
+            <h2 className="font-shouIders">SUVs</h2>
+            <p className="font-lexend">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+            <a className="font-lexend" href="https://www.webmotors.com.br/suvs" target="_blank" >ver mais</a>
       </div>
       <div className="bg-dark-green">
           <img src={iconLuxosos} alt="" />
-          <h2>Luxosos</h2>
-          <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
-          <a href="https://www.webmotors.com.br/catalogo/carroceria/coupe/" target="_blank">ver mais</a>
+          <h2 className="font-shouIders">Luxosos</h2>
+          <p className="font-lexend">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+          <a className="font-lexend"  href="https://www.webmotors.com.br/catalogo/carroceria/coupe/" target="_blank">ver mais</a>
       </div>
       </section>
     </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,11 @@ export default {
       'offwhite' : 'rgba(255, 255, 255, 0.75)',
       'white' :  '#fff',
       'offwhitebg' : '#f2f2f2'
-    } ,
-    fontFamily : {
-      'showIders': '"Big Shoulders Display"',
-      'lexend': '"Lexend Deca'
+    },
+
+    fontFamily: {
+      'shouIders': '"Big Shoulders Display"',
+      'lexend': '"Lexend Deca"'
     }
   },
   plugins: [],
