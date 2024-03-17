d91037ddfdefbf4492724f7ad6e22d19b94dd75c
gabriel lima <gabriellima140806@gmail.com>
2024-03-17 18:57:49 -0300

    projeto finalizadp

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import Luxosos from "./components/Luxosos"
 export default function App() {
   return (
     <main className="min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justify-center items-center " >
-      <section className="lg:flex max-w-{920px}">
+      <div className="lg:flex max-w-{920px}">
       <Sedans/><Suvs/><Luxosos/>  
-      </section>
+      </div>
     </main>
   )
 }
--- src/components/Luxosos.jsx
+++ src/components/Luxosos.jsx
@@ -2,12 +2,12 @@ import iconLuxosos from "../assets/icon-luxury.svg"
 
 export default function Luxosos() {
   return (
-    <div className="bg-dark-green p-12 max-lg:rounded-b-lg
+    <section className="bg-dark-green p-12 max-lg:rounded-b-lg
     lg:rounded-r-lg ">
        <img src={iconLuxosos} alt=""  className="mb-7"/>
        <h2 className="font-shouIders  mb-7 uppercase text-4xl font-bold text-offwhitebg">Luxosos</h2>
        <p className="font-lexend  mb-7 text-offwhite lg:mb-20">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
        <a className="font-lexend mb-7 bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-black-green border-2 border-offwhitebg hover:bg-dark-green hover:text-offwhitebg"  href="https://www.webmotors.com.br/catalogo/carroceria/coupe/" target="_blank">ver mais</a>
-   </div>
+   </section>
   )
 }
